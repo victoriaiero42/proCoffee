68e80b7d4b9ff8ba2a0396030f03e89848cdf51e
Victoria <victoriaiero@gmail.com>
2020-10-16 10:21:15 +0300

    menu

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import Menu from '../Menu/Menu'
+import MediaCard from '../Guides/Guides'
+import { Switch, Route } from 'react-router-dom'
+import TabsWrappedLabel from '../ArticleTitle/ArticleTitles'
 
 function App() {
   return (
     <div className="App">
-      bnsdkjgbvdlk
+      <Switch>
+        <Route exact path="/">
+          {/* <Menu /> */}
+        </Route>
+        <Route exact path="/guides">
+          <MediaCard />
+        </Route>
+        <Route exact path="/profile">
+          <TabsWrappedLabel />
+        </Route>
+      </Switch>
+      <Menu />
+
     </div>
   );
 }
