4276eac829f2e12336dd4eec7b1c34e92ad4b397
Igor <kovinko.igor@gmail.com>
2020-10-16 20:04:51 +0300

    redux + saga auth add

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -1,31 +1,35 @@
 import React from 'react';
-import Menu from '../Menu/Menu'
-import MediaCard from '../Guides/Guides'
-import { Switch, Route } from 'react-router-dom'
+import Menu from '../Menu/Menu';
+import MediaCard from '../Guides/Guides';
+import { Switch, Route } from 'react-router-dom';
 // import TabsWrappedLabel from '../ArticleTitle/ArticleTitles'
-import CenteredGrid from '../ArticleTitle/ArticleTitles'
-import Panel from '../Panel'
-import Authorization from '../Authorization'
+import CenteredGrid from '../ArticleTitle/ArticleTitles';
+import Panel from '../Panel';
+import { Provider } from 'react-redux';
+import Authorization from '../Authorization';
+import store from '../../redux/store';
 
 function App() {
   return (
     <div className="App">
-      <Authorization />
-      <Switch>
-        <Route exact path="/">
-          {/* <Menu /> */}
-        </Route>
-        <Route exact path="/guides">
-          <MediaCard />
-        </Route>
-        <Route exact path="/profile">
-          <CenteredGrid />
-        </Route>
-        <Route exact path="/search">
-          <Panel />
-        </Route>
-      </Switch>
-      <Menu />
+      <Provider store={store}>
+        <Authorization />
+        <Switch>
+          <Route exact path="/">
+            {/* <Menu /> */}
+          </Route>
+          <Route exact path="/guides">
+            <MediaCard />
+          </Route>
+          <Route exact path="/profile">
+            <CenteredGrid />
+          </Route>
+          <Route exact path="/search">
+            <Panel />
+          </Route>
+        </Switch>
+        <Menu />
+      </Provider>
     </div>
   );
 }
