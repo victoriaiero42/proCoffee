3543692b9ef6f52c111bdac341f0442d126df689
Igor <kovinko.igor@gmail.com>
2020-10-20 19:24:02 +0300

    some changes on front

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -11,7 +11,6 @@ import GuideList from '../GuideList/GuideList';
 import Authorization from '../Authorization';
 import store from '../../redux/store';
 import OneGuide from '../oneGuide/OneGuide';
-import LoginForm from '../LoginForm';
 import LogoutButton from '../LogoutButton';
 
 import SignIn from '../SignIn/Signin'
@@ -25,8 +24,7 @@ function App() {
   return (
     <div className="App">
       <Provider store={store}>
-        <LoginForm />
-        <LogoutButton />
+        {/* <LogoutButton /> */}
         <Switch>
           {/* <Panel /> */}
           <Route exact path="/guides">
@@ -37,10 +35,12 @@ function App() {
             <Panel />
             <OneGuide />
           </Route>
+          <Route exact path="/register">
+            <Authorization />
+          </Route>
           <Route exact path="/profile">
             <Panel />
 
-            {/* <Authorization /> */}
             <SignIn />
 
             {/* <Profile /> */}
--- front/src/components/SignIn/Signin.js
+++ front/src/components/SignIn/Signin.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import { startLoginSaga } from '../../redux/actions/authActions';
+
+import GoogleAuth from '../GoogleAuth';
+
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
+// import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
@@ -34,7 +41,22 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function SignIn() {
+  const dispatch = useDispatch();
+  const [formData, setFormData] = useState({});
   const classes = useStyles();
+
+  async function handleClick(e) {
+    e.preventDefault();
+    dispatch(startLoginSaga(formData));
+  }
+
+  const handleChange = (e) => {
+    e.persist();
+    setFormData((state) => ({
+      ...state,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
   return (
     <Container component="main" maxWidth="xs">
@@ -46,7 +68,7 @@ export default function SignIn() {
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
-        <form className={classes.form} noValidate>
+        <form onChange={(e) => handleChange(e)} className={classes.form} noValidate>
           <TextField
             variant="outlined"
             margin="normal"
@@ -74,6 +96,7 @@ export default function SignIn() {
             label="Remember me"
           />
           <Button
+            onClick={(e) => handleClick(e)}
             type="submit"
             fullWidth
             variant="contained"
@@ -84,21 +107,21 @@ export default function SignIn() {
           </Button>
           <Grid container>
             <Grid item xs>
-              <Link href="#" variant="body2">
+              <Link to={"/restore"}>
                 Forgot password?
               </Link>
             </Grid>
             <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
+              <Link to={"/register"}>
+                Don't have an account? Sign Up
               </Link>
             </Grid>
           </Grid>
         </form>
+        <GoogleAuth />
       </div>
       <Box mt={8}>
-        {/* <Copyright /> */}
       </Box>
-    </Container>
+    </Container >
   );
 }
