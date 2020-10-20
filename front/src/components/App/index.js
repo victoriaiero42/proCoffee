2ff768c61e19df3015d20bbb8e657e52259c3982
Igor <kovinko.igor@gmail.com>
2020-10-20 09:28:58 +0300

    front app some change by seslint

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
+import { Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Menu from '../Menu/Menu';
 // import DragAndDrop from '../DragAndDrop';
-import { Switch, Route } from 'react-router-dom';
 import Panel from '../Panel';
-import { Provider } from 'react-redux';
 // import Menu from '../Menu/Menu';
 import GuideList from '../GuideList/GuideList';
 // import Panel from '../Panel';
 import Authorization from '../Authorization';
 import store from '../../redux/store';
 import OneGuide from '../oneGuide/OneGuide';
-
-
 
 function App() {
   return (
