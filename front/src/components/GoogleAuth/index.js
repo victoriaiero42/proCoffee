e7430ae7c716ddb5b45962de17ca296c49729e70
Igor <kovinko.igor@gmail.com>
2020-10-23 13:25:16 +0300

    fix mailer

--- front/src/components/GoogleAuth/index.js
+++ front/src/components/GoogleAuth/index.js
@@ -11,7 +11,7 @@ function GoogleAuth() {
   const dispatch = useDispatch();
 
   async function handleClick() {
-    window.open('/google', '_self');
+    window.open('/google');
     dispatch(startGoogleAuthenticateSaga());
   }
 
