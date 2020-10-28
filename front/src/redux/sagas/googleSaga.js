5e6f6c908252aa515a72c15339a802190b70481e
Igor <kovinko.igor@gmail.com>
2020-10-29 02:00:02 +0300

    still fix google auth_2.0

--- front/src/redux/sagas/googleSaga.js
+++ front/src/redux/sagas/googleSaga.js
@@ -1,15 +1,18 @@
 import { takeEvery, put, call } from 'redux-saga/effects';
+import { useHistory } from 'react-router-dom';
 
 import { START_GOOGLE } from '../actionTypes';
 
 import { authenticateGoogleUser } from '../actions/authActions';
 
 async function fetchGoogle() {
+  const history = useHistory();
   console.log('фетч ту гугл начало');
   const request = await fetch('/api/goodGoogle');
   console.log(request, 'запрос на бэк');
   const response = await request.json();
   console.log(response, 'ответ бэка!!');
+  history.push('/top');
   return response;
 }
 
