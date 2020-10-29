9c726dccd8f4aa1aed737993a436f70f4636a000
Igor <kovinko.igor@gmail.com>
2020-10-30 01:18:03 +0300

    passport fix

--- front/src/components/GoogleAuth/index.js
+++ front/src/components/GoogleAuth/index.js
@@ -4,18 +4,21 @@ import { Link } from 'react-router-dom';
 import './style.css';
 
 function GoogleAuth() {
+
   return (
     <>
       {/* <li><a href="http://localhost:3001/google">Login With Google</a></li> */}
 
-      <Link to="/google">
-        <div className="google-btn">
-          <div className="google-icon-wrapper">
-            <img className="google-icon-svg" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="Google logo" />
-          </div>
-          <p className="btn-text">Используй силу ГУГЛА!</p>
+      {/* <Link to="/google"> */}
+      <a href="http://localhost:3001/google"></a>
+      <div className="google-btn">
+        <div className="google-icon-wrapper">
+          <img className="google-icon-svg" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="Google logo" />
         </div>
-      </Link>
+        <p className="btn-text">Используй силу ГУГЛА!</p>
+      </div>
+    </a>
+      {/* </Link> */ }
 
     </>
   );
